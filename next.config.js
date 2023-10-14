4e22a7cc7e1078d50a15b088609e35f67acb337f
Angel <a.rioncervi@gmail.com>
2023-10-14 22:58:36 +0200

    WIP work svg

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgo: false,
+          },
+        },
+      ],
+    })
+    return config
+  },
+}
 
 module.exports = nextConfig
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,9 +1,49 @@
-import styles from './styles.module.scss'
+'use client'
+
+import anime from 'animejs'
+import styles from './style.module.scss'
+import WorkTimeline from '@components/WorkTimeline/WorkTimeline'
+import { useEffect } from 'react'
 
 export default function Work() {
+
+  function handlePlusClick({name, x, y}) {
+    console.log({name, x, y})
+  }
+
+  useEffect(() => {
+    // different targets to deffer animation
+    anime({
+      targets: `.${styles.workContainer} path`,
+      strokeDashoffset: [anime.setDashoffset, 0],
+      easing: 'easeInOutSine',
+      duration: (el, i) => {
+        if (el.classList.contains('bar')) {
+          return 200
+        } else if (el.classList.contains('main-line')) {
+          return 3000
+        }
+
+        return 1500
+      },
+      delay: (el, i) => {
+        console.log(i)
+
+        if (el.classList.contains('bar')) {
+          return i * 45
+        } else if (el.classList.contains('main-line')) {
+          return 100
+        }
+        return 150
+      },
+      direction: 'forward',
+      //loop: true,
+    })
+  })
+
   return (
-    <div>
-      <div>work</div>
+    <div className={styles.workContainer}>
+      <WorkTimeline onPlusClick={handlePlusClick} />
     </div>
   )
 }
