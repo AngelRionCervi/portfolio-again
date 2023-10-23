3cad973a14c6017d44889f8d57d5d5da20be98bc
Angel <a.rioncervi@gmail.com>
2023-10-23 22:05:07 +0200

    done desktop modal

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -2,47 +2,47 @@
 
 import styles from './style.module.scss'
 import WorkTimeline from '@components/WorkTimeline/WorkTimeline'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import workTimelineAnimation, { stopAnimation, totalDuration } from '@components/WorkTimeline/workTimelineAnimation'
 import { getSeenWorkPage, setSeenWorkPage } from '@lib/sessionStorage'
 import WorkTimelineModal from '@/components/Modal/WorkTimelineModal'
 
 export default function Work() {
-  const [selectedCompany, setSelectedCompany] = useState<CompanyProps | null>(null);
+  const [selectedCompany, setSelectedCompany] = useState<CompanyProps | null>(null)
+  const [isMounted, setIsMounted] = useState(false)
+
+  const intervalRef = useRef(0)
 
   function handlePlusClick({ name, x, y, size }: PlusPayload) {
-    closeModal();
+    closeModal()
     setTimeout(() => {
-      setSelectedCompany({ name, position: { x, y }, size });
+      setSelectedCompany({ name, position: { x, y }, size })
     })
   }
 
-  let setSeenTimeout: number;
-
   useEffect(() => {
     if (getSeenWorkPage() !== 'true') {
-      workTimelineAnimation(`.${styles.workContainer}`);
-      setSeenTimeout = window.setTimeout(() => {
-        setSeenWorkPage();
+      workTimelineAnimation(`.${styles.workContainer}`)
+      intervalRef.current = window.setTimeout(() => {
+        setSeenWorkPage()
       }, totalDuration)
     }
+    setIsMounted(true)
 
     return () => {
-      clearTimeout(setSeenTimeout);
-      stopAnimation();
+      clearTimeout(intervalRef.current)
+      stopAnimation()
     }
   }, [])
 
   function closeModal() {
-    setSelectedCompany(null);
+    setSelectedCompany(null)
   }
 
   return (
     <div className={styles.workContainer}>
-      {selectedCompany && (
-        <WorkTimelineModal company={selectedCompany} closeModal={closeModal} />
-      )}
-      <div className={styles.workContainerInner}>
+      {selectedCompany && <WorkTimelineModal company={selectedCompany} closeModal={closeModal} />}
+      <div style={{ visibility: isMounted ? 'visible' : 'hidden' }} className={styles.workContainerInner}>
         <WorkTimeline onPlusClick={handlePlusClick} />
       </div>
     </div>
