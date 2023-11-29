05fcca8350a72779b993f3a0ad95c5df93af5ca0
Angel <a.rioncervi@gmail.com>
2023-11-29 18:21:45 +0100

    mobile article year selector

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,13 +1,25 @@
+'use client'
+
 import BlogYearSelectorDesktop from '@components/BlogYearSelector/BlogYearSelectorDesktop/BlogYearSelectorDesktop'
+import BlogYearSelectorMobile from '@components/BlogYearSelector/BlogYearSelectorMobile/BlogYearSelectorMobile'
 import styles from './styles.module.scss'
+import { useDevice } from '@lib/hooks/useDevice'
 
 export default function Blog() {
+  const isMobile = useDevice()
+
   return (
-    <div className={styles.container}>
-      <div className={styles.yearSelectorContainer}>
-        <BlogYearSelectorDesktop />
-      </div>
-      <div className={styles.articlesContainer}>-articles-</div>
+    <div className={isMobile ? styles.containerMobile : styles.containerDesktop}>
+      {isMobile ? (
+        <div className={styles.yearSelectorContainerMobile}>
+          <BlogYearSelectorMobile />
+        </div>
+      ) : (
+        <div className={styles.yearSelectorContainerDesktop}>
+          <BlogYearSelectorDesktop />
+        </div>
+      )}
+      <div className={isMobile ? styles.articlesContainerMobile : styles.articlesContainerDesktop}>-articles-</div>
     </div>
   )
 }
--- src/components/BlogYearSelector/BlogYearSelectorMobile/BlogYearSelectorMobile.tsx
+++ src/components/BlogYearSelector/BlogYearSelectorMobile/BlogYearSelectorMobile.tsx
@@ -1,6 +1,61 @@
+import { useEffect, useState } from 'react'
+import styles from './styles.module.scss'
+import CornerTriangle from '@assets/icons/corner-triangle.svg'
+import anime from 'animejs'
+import { useAfterMountEffect } from '@/lib/hooks/useAfterMountEffect'
 
 export default function BlogYearSelectorMobile() {
+  const years = [2024, 2023, 2022]
+  const buttonHeight = 45
+  const animDuration = 150
+
+  const [currentYear, setCurrentYear] = useState<number | null>(null)
+  const [selectState, setSelectState] = useState<boolean>(false)
+
+  useEffect(() => {
+    setCurrentYear(years[0])
+  }, [])
+
+  useAfterMountEffect(() => {
+    anime({
+      targets: `.${styles.container}`,
+      height: [buttonHeight, years.length * buttonHeight],
+      duration: animDuration,
+      easing: 'linear',
+      direction: selectState ? 'forward' : 'reverse',
+    })
+
+    anime({
+      targets: `.${styles.arrowDown}`,
+      rotate: [-135, 45],
+      duration: animDuration,
+      easing: 'linear',
+      direction: selectState ? 'forward' : 'reverse',
+    })
+  }, [selectState])
+
+  function changeYear(year: number) {
+    setCurrentYear(year)
+    setSelectState(false)
+  }
+
   return (
-    <div>BlogYearSelectorMobile</div>
+    <div className={styles.container}>
+      <button onClick={() => setSelectState(!selectState)} className={styles.currentYearButton}>
+        <p className={styles.currentYear}>{currentYear}</p>
+        <CornerTriangle width="10px" height="10px" className={styles.arrowDown} />
+      </button>
+      <div className={styles.otherYearsContainer}>
+        {years
+          .filter((year) => year !== currentYear)
+          .map((year) => {
+            return (
+              <button onClick={() => changeYear(year)} key={year} className={styles.otherYear}>
+                {year}
+              </button>
+            )
+          })}
+      </div>
+    </div>
   )
 }
