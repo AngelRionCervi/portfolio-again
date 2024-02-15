7d4a8c42a1c68e2ccfeb56b7ec85bfeb880e107a
Angel <a.rioncervi@gmail.com>
2024-02-15 18:01:33 +0100

    css fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react';
-import { Karla } from 'next/font/google'
 import './style.scss'
 import '@styles/highlightThemes/panda-syntax-light.css'
 import '@styles/highlightThemes/panda-syntax-dark.css'
 import LayoutCheck from '@components/Layout/LayoutCheck'
 import ContextReducer from '@context/ContextReducer'
-
-const karla = Karla({ subsets: ['latin'], weight: "400" })
 
 export const metadata: Metadata = {
   title: 'Angel Rion - Cervi',
@@ -17,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={karla.className}>
+      <body>
         <ContextReducer>
           <LayoutCheck>{children}</LayoutCheck>
         </ContextReducer>
